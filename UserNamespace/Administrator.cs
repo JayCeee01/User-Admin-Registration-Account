e77ab4a7f6634e02be7097dd43076d445a0e1cd7
JayCee <158656310+JayCeee01@users.noreply.github.com>
2024-10-13 20:23:59 +0800

    Add project files.

---
+++ UserNamespace/Administrator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserNamespace
+{
+    internal class Administrator : User
+    {
+        private string admin_name;
+
+        public Administrator(string name, string id, string pass) : base(id, pass)
+        {
+            this.admin_name = name;
+        }
+
+        public override void updatePassword(string newPassword)
+        {
+            Console.WriteLine($"Update succesful. Your new password is: {newPassword}");
+            this.user_password = newPassword;
+        }
+
+        public void updateAdminName(string name)
+        {
+            Console.WriteLine($"Update succesful. Your new password is: {name}");
+            this.admin_name = name;
+        }
+    }
+}
---
+++ UserNamespace/User.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserNamespace
+{
+    internal class User
+    {
+        private string user_id;
+        protected string user_password;
+
+
+        public User(string id, string pass)
+        {
+            this.user_id = id;
+            this.user_password = pass; 
+
+            //verifyLogin(id, pass);
+        }
+
+        public bool verifyLogin(string id, string pass)
+        {
+            bool verify;
+
+            if (id.Equals("admin") && pass.Equals("pass")){
+                verify = true;
+                Console.WriteLine("Login Successfully.");
+                return verify;
+            }
+            else
+            {
+                verify = false;
+                Console.WriteLine("Login Failed. Incorrect username or password.");
+                return verify;
+            }
+        }
+
+        public virtual void updatePassword(string newPassword)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Enter your username: ");
+                string id = Console.ReadLine();
+                string pass = Console.ReadLine();
+
+                if (id.Equals("") && pass.Equals(""))
+                {
+                    User user = new User(id, pass);
+                    bool verifyUser = user.verifyLogin(id, pass);
+
+                    if (verifyUser == true) 
+                    {
+                        Console.WriteLine("Registering Account... ");
+
+                    }
+
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Please fill out the username and password");
+                    continue;
+                }
+
+            }
+            
+        }
+    }
+}
